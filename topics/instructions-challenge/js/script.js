706c5542f80641239e4815083a4ad8f57bd92fa9
Skyla Trousdale <skylatrousdale@gmail.com>
2025-09-11 14:05:59 -0700

    More details !
    Sunset with clouds, details with the sun, organizing and comments/title

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -24,7 +24,9 @@ drawMountain2();
 drawMountain3();
 drawGrass();
 drawPond();
+drawCloudSun();
 drawCloud();
+
 }
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
@@ -33,14 +35,28 @@ function drawSky(){
     //the sky
     push();
     background(74, 192, 247)
+    noStroke();
+    fill("#b2b2ffff");
+    rect(0, 100, 680, 480);
+    fill("#d8b2ffff");
+    rect(0, 170, 680, 480);
+    fill("#ffb2f9ff");
+    rect(0, 230, 680, 480);
+    fill("#ffa2b8ff");
+    rect(0, 280, 680, 480);
+    fill("#fa8686ff");
+    rect(0, 320, 680, 480);
+    fill("#f9785bff");
+    rect(0, 340, 680, 480);
     pop();
 }
 function drawSun() {
  //the sun
  push();
- noStroke();
- fill("#ffec7dff")
-ellipse(100, 80, 100)
+ stroke("#f8b45aff");
+ strokeWeight(20)
+ fill("#eef066ff")
+ellipse(210, 220, 180)
 pop();
 } 
 function drawMountain1(){
@@ -53,21 +69,21 @@ pop();
 function drawMountain2(){
     push();
 noStroke();
-fill("#ffffffff")
+fill("#f2f4f9ff")
 ellipse(500,450,250,550)
 pop();
 }
 function drawMountain3(){
     push();
 noStroke();
-fill("#ffffffff");
+fill("#ecf4f5ff");
 ellipse(170,500,250,550);
 pop();
 }
 function drawGrass(){
     push();
-    stroke("#055e05ff");
-    strokeWeight(10);
+    stroke("#0d750dff");
+    strokeWeight(4);
     fill("#0a7d0aff");
     rect(-20, 350, 680, 480);
     pop();
@@ -81,13 +97,37 @@ function drawPond(){
     ellipse(310,420,600,100);
     pop();
 }
+function drawCloudSun(){
+push();
+noStroke();
+ fill("#fa86baa5");
+ellipse(550, 125, 400, 60);
+
+noStroke();
+ fill("#fa86865e");
+ellipse(100, 275, 600, 60);
+
+noStroke();
+ fill("#eeabf390");
+ellipse(150, 55, 250, 50);
+
+
+pop();
+}
 function drawCloud(){
 push();
 noStroke();
 fill(255);
-ellipse(100, 100, 100, 100);
-ellipse(180, 80, 100, 100);
-ellipse(160, 120, 60, 60);
-ellipse(190, 130, 60, 60);
+ellipse(550, 120, 400, 50);
+
+noStroke();
+fill(255);
+ellipse(100, 270, 600, 50);
+
+noStroke();
+fill(255);
+ellipse(150, 50, 250, 40);
+
+
 pop();
 }
