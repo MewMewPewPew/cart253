a9d0eec419bf70621e9cab1084731f7fa5d6f9c2
Ash <131301186+MewMewPewPew@users.noreply.github.com>
2025-09-18 13:59:47 -0400

    setting up the week 3 class
    (opening the "thinking like a computer with variables")

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Landscape
- * Tasha and Ima 
+ * Snowy Sunset
+ * Ash and Ima 
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Beautiful sunset at the pond near the snowy mountain! So relaxing and fun and exciting :D
  */
 
 "use strict";
@@ -12,7 +11,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-createCanvas(640, 480)
+createCanvas(640, 480);
 }
 
 function draw(){
@@ -32,10 +31,10 @@ drawCloud();
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function drawSky(){
-    //the sky
+    //the sunset
     push();
-    background(74, 192, 247)
-    noStroke();
+    background(74, 192, 247);
+    noStroke(); 
     fill("#b2b2ffff");
     rect(0, 100, 680, 480);
     fill("#d8b2ffff");
@@ -48,32 +47,36 @@ function drawSky(){
     rect(0, 320, 680, 480);
     fill("#f9785bff");
     rect(0, 340, 680, 480);
+    //we added different hues of the sunset
     pop();
 }
 function drawSun() {
  //the sun
  push();
  stroke("#f8b45aff");
- strokeWeight(20)
- fill("#eef066ff")
-ellipse(210, 220, 180)
+ strokeWeight(20);
+ fill("#eef066ff");
+ellipse(210, 220, 180);
 pop();
 } 
-function drawMountain1(){
+function drawMountain1(){ 
+    //the big middle snowy mountain
     push();
 noStroke();
-fill("#d8eff7ff")
-ellipse(350,350,250,550)
+fill("#d8eff7ff");
+ellipse(350,350,250,550);
 pop();
 }
 function drawMountain2(){
+    // the right mountain
     push();
 noStroke();
-fill("#f2f4f9ff")
-ellipse(500,450,250,550)
+fill("#f2f4f9ff");
+ellipse(500,450,250,550);
 pop();
 }
 function drawMountain3(){
+    // the left mountain
     push();
 noStroke();
 fill("#ecf4f5ff");
@@ -81,6 +84,7 @@ ellipse(170,500,250,550);
 pop();
 }
 function drawGrass(){
+    // the grass
     push();
     stroke("#0d750dff");
     strokeWeight(4);
@@ -91,13 +95,14 @@ function drawGrass(){
 function drawPond(){
     //the Pond
     push();
-    stroke(61, 164, 212)
-    strokeWeight(5)
+    stroke(61, 164, 212);
+    strokeWeight(5);
     fill(74, 192, 247);
     ellipse(310,420,600,100);
     pop();
 }
 function drawCloudSun(){
+    // the reflection of the sunset on the clouds
 push();
 noStroke();
  fill("#fa86baa5");
@@ -115,19 +120,21 @@ ellipse(150, 55, 250, 50);
 pop();
 }
 function drawCloud(){
+    //the clouds
+    
 push();
 noStroke();
 fill(255);
 ellipse(550, 120, 400, 50);
-
+// the right one
 noStroke();
 fill(255);
 ellipse(100, 270, 600, 50);
-
+// the left one
 noStroke();
 fill(255);
 ellipse(150, 50, 250, 40);
-
+//the small one 
 
 pop();
 }
